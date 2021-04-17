7a4070379ab071ebf28d06cfff36bc96dbb29281
DevJewel <58494211+DevJewel143@users.noreply.github.com>
2021-04-17 23:35:45 +0600

    Update set.py

--- robot test/set.py
+++ robot test/set.py
@@ -1,18 +1,24 @@
 
 
+
+
 import time
 from adafruit_servokit import ServoKit
 
-yes = ServoKit(channels=16)
-yes.servo[0].angle = 0
-time.sleep(0.5)
-for i in range(0,35):
-        yes.servo[0].angle = i
-        time.sleep(0.5)
+init  = [0,  20,  170, 180, 0, 10, 0, 0, 0, 0, 0, 0] 
+limit = [35, 180, 0, 180, 140, 180, 180, 180, 180, 180, 180, 180]
 
-time.sleep(0.5)
-for i in range(34,0,-1):
-        yes.servo[0].angle = i
-        time.sleep(0.5)
+h = ServoKit(channels=16)
+def SetPosition :
+	for i in range(0, 12):
+		h.servo[i].angle = init[i]
+
+
+while True:
+	n = (int)(input("servo no : "))
+	d = (int)(input("enter degree : "))
+	h.servo[n].angle = d
+
+
 
 print('done')
